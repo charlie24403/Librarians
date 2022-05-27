2709ae4163f0b6dcf8bc18791c6f84129d0f53cf
charlie24403 <2022php42-student026@la-study.com>
2022-05-27 14:11:32 +0900

    確認画面でのエラーの修正

--- Development/src/app/laravel/resources/views/users/form_confirm.blade.php
+++ Development/src/app/laravel/resources/views/users/form_confirm.blade.php
@@ -2,8 +2,7 @@
 
 @section('content')
 <h3>確認</h3>
-<form method="post" action="{{ route('users.send', $user->id) }}">
-	@method('patch')
+<form method="post" action="{{ route('users.send') }}">
 	@csrf
 	<label>名前</label>
 	<div>
