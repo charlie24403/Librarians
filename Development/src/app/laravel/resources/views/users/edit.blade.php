2d8ab378d6045c93a8a53e129e734eec803b4e9b
Ryoto <c0118128f5@edu.teu.ac.jp>
2022-05-30 14:10:48 +0900

    [add]

--- Development/src/app/laravel/resources/views/users/edit.blade.php
+++ Development/src/app/laravel/resources/views/users/edit.blade.php
@@ -11,20 +11,22 @@
 <!-- content -->
 @section('content')
 @include('commons/flash')
-<form action="{{ route('users.update_post', $user->id)}}" method="post">
-    @include('users/commons/form')
-    <p>
-        <input class="btn btn-primary" type="submit" value="登録"/>
-    </p>
+    <div class="input-form">
+        <form action="{{ route('users.update_post', $user->id)}}" method="post">
+            @include('users/commons/form')
+            <p>
+                <input class="btn btn-primary" type="submit" value="登録"/>
+            </p>
 
-    <p>
-        <input class="btn btn-primary" type="submit" value="更新"/>
-    </p>
+            <p>
+                <input class="btn btn-primary" type="submit" value="更新"/>
+            </p>
 
-    <p>
-        <a href="{{ route('users.show', $user->id) }}">
-            <button type="button">キャンセル</button>
-        </a>
-    </p>
-</form>
+            <p>
+                <a href="{{ route('users.show', $user->id) }}">
+                    <button type="button">キャンセル</button>
+                </a>
+            </p>
+        </form>
+    </div>
 @endsection
