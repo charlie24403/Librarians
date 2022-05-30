dca04ff4bde0c455b7676e91e67193af6b4545dd
charlie24403 <2022php42-student026@la-study.com>
2022-05-30 16:23:50 +0900

    2つのボタンのエラーを修正

--- Development/src/app/laravel/resources/views/users/edit.blade.php
+++ Development/src/app/laravel/resources/views/users/edit.blade.php
@@ -11,16 +11,15 @@
 <!-- content -->
 @section('content')
 <div class = "input-page">
-    @include('commons/flash')
-    <form action="{{ route('users.update_post', $user->id)}}" method="post">
-        @include('users/commons/form')
+@include('commons/flash')
+<form action="{{ route('users.update_post', $user->id)}}" method="post">
+    @csrf
+    @include('users.commons.form')
 
-        <button type="submit" class="btn btn-primary">登録</button>
+    <button type="submit" class="btn btn-primary">更新</button>
 
-        <button type="submit" class="btn btn-primary">更新</button>
-
-        <a href="{{ route('users.show', $user->id) }}">
-                <button type="button">キャンセル</button>
-        </a>
-    </form>
+    <a href="{{ route('users.show', $user->id) }}">
+            <button type="button">キャンセル</button>
+    </a>
+</form>
 </div>
