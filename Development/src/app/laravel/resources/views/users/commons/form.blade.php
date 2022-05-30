d09aab930d059b04861afad5ae569b2fd025e6c1
charlie24403 <2022php42-student026@la-study.com>
2022-05-30 16:38:51 +0900

    更新フォームに現在の情報を保持するように

--- Development/src/app/laravel/resources/views/users/commons/form.blade.php
+++ Development/src/app/laravel/resources/views/users/commons/form.blade.php
@@ -1,15 +1,15 @@
 <div class = "form">
-    @csrf
+    
     <dl>
         <dt>名前</dt>
-        <dd><input type="text" name="name" value="{{ old('name') }}"></dd>
+        <dd><input type="text" name="name" value="{{ $user['name'] }}"></dd>
         <dt>住所</dt>
-        <dd><input type="text" name="address" value="{{ old('address') }}"></dd>
+        <dd><input type="text" name="address" value="{{ $user['address'] }}"></dd>
         <dt>電話番号</dt>
-        <dd><input type="number" name="tel" value="{{ old('tel') }}"></dd>
+        <dd><input type="number" name="tel" value="{{ $user['tel'] }}"></dd>
         <dt>メールアドレス</dt>
-        <dd><input type="email" name="mail" value="{{ old('mail') }}"></dd>
+        <dd><input type="email" name="mail" value="{{ $user['mail'] }}"></dd>
         <dt>生年月日</dt>
-        <dd><input type="date" name="birth" value="{{ old('birth') }}"></dd>
+        <dd><input type="date" name="birth" value="{{ $user['birth'] }}"></dd>
     </dl>
 </div>
